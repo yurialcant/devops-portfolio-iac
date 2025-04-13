f7f9a076acc6bdeac3ecd685f50a4de44d8d489a
yurialcant <yurialcant@gmail.com>
2025-04-13 16:32:24 -0300

    ajuste do codigo teste_app

--- app/flask_hello/test_app.py
+++ app/flask_hello/test_app.py
@@ -4,4 +4,6 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Ola" in response.data
+    assert b"Hello, Terraform!" in response.data, f"Esperado 'Hello, Terraform!', mas obtido {response.data.decode()}"
+
+
