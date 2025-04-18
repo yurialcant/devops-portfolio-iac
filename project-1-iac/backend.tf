47945f27256fe42ef382941e2762bb743c879805
yurialcant <yurialcant@gmail.com>
2025-04-18 16:14:37 -0300

    mudança no path do ignore para o backend

---
+++ project-1-iac/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "backend-iac-1"  
+    key            = "terraform.tfstate"
+    region         = "us-east-1"  
+    encrypt        = true  
+    dynamodb_table  = "terraform-lock-table"  
+  }
+}
